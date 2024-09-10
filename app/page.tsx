72351f64378cfc970bffa38eb47c128b706962b6
shirlyyang-tcc <69131160+shirlyyang-tcc@users.noreply.github.com>
2024-09-10 06:43:29 +0000

    update

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import FAQ from './components/Faq';
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-8">
+      <span>abcdefg</span>
       <Resume data={resumeData} />
       <FAQ />
     </main>
